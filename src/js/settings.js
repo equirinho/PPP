328ba2380d262021a97ba67c83bf301440a51360
equirinho <erickinacio73@gmail.com>
2022-06-08 23:56:24 -0300

    primeiras telas do projeto produzidas

---
+++ src/js/settings.js
@@ -0,0 +1,4 @@
+var CLIENT_ID = 'knrwYR4lQAoXrTUPGiAaGC5XbVhtp69IUYirYUYt';
+var REDIRECT_URI = 'http://127.0.0.1:5500/pages/';
+var SUAP_URL = 'https://suap.ifrn.edu.br';
+var SCOPE = 'identificacao email documentos_pessoais';
